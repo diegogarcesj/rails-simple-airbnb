cb896919c994bb3414426c327e127449abf452b6
Diego Garces Jimenez <diegogarcesj1@gmail.com>
2021-08-02 16:54:37 -0400

    Edit and update action with refactored views

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,6 +1,11 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
+
   def index
     @flats = Flat.all
+  end
+
+  def show
   end
 
   def new
@@ -16,8 +21,23 @@ class FlatsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @flat.update(flat_params)
+
+    redirect_to flats_path
+  end
+
+  def destroy
+  end
 
   private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
 
   def flat_params
     params.require(:flat).permit(:name, :address, :description, :picture_url, :number_of_guests, :price_per_night)
